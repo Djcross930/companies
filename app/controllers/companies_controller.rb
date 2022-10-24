9104929f23c8bba142fa5843e16f373570a1cfa3
Dan Cross <dancross96@gmail.com>
2022-10-23 22:29:27 -0500

    added show action for company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,9 @@ class CompaniesController < ApplicationController
     companies = Company.all
     render json: companies.as_json
   end
+
+  def show
+    company = Company.find_by(id: params[:id])
+    render json: company.as_json
+  end
 end
